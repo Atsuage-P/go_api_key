31c74f319b4d349db8d48d01aa503ed6c49d9b7b
atsuage <atsuage.programmer@gmail.com>
2024-07-27 12:46:21 +0900

    リファクタ

--- controller/cotroller.go
+++ controller/cotroller.go
@@ -4,6 +4,7 @@ import (
 	"api_key_test/env"
 	"api_key_test/oapi"
 	"api_key_test/structlog"
+	"errors"
 	"log/slog"
 	"net/http"
 
@@ -27,20 +28,14 @@ func (a *APIController) DeleteNumber(c echo.Context, params oapi.DeleteNumberPar
 	ctx := c.Request().Context()
 	ctx = structlog.WithValue(ctx, "params", params)
 
-	cfg := env.LoadEnv()
-	if params.XAPIKEY != cfg.APIKey {
+	if err := validateAPIKey(params.XAPIKEY); err != nil {
 		slog.ErrorContext(ctx, "Wrong API Key", "method", "DeleteNumber")
 		return c.JSON(http.StatusUnauthorized, map[string]string{"message": http.StatusText(http.StatusUnauthorized)})
 	}
 
-	req := new(oapi.NumberReq)
-	if err := c.Bind(req); err != nil {
-		slog.ErrorContext(ctx, "Request Bind Error", "method", "DeleteNumber")
-		return c.JSON(http.StatusBadRequest, map[string]string{"message": http.StatusText(http.StatusBadRequest)})
-	}
-
-	if req.Num == nil {
-		slog.ErrorContext(ctx, "Wrong Request Body", "method", "PostNumber")
+	req, err := validateRequestBody(c)
+	if err != nil {
+		slog.ErrorContext(ctx, err.Error(), "method", "DeleteNumber")
 		return c.JSON(http.StatusBadRequest, map[string]string{"message": http.StatusText(http.StatusBadRequest)})
 	}
 
@@ -52,23 +47,54 @@ func (a *APIController) PostNumber(c echo.Context, params oapi.PostNumberParams)
 	ctx := c.Request().Context()
 	ctx = structlog.WithValue(ctx, "params", params)
 
-	cfg := env.LoadEnv()
-	if params.XAPIKEY != cfg.APIKey {
+	if err := validateAPIKey(params.XAPIKEY); err != nil {
 		slog.ErrorContext(ctx, "Wrong API Key", "method", "PostNumber")
 		return c.JSON(http.StatusUnauthorized, map[string]string{"message": http.StatusText(http.StatusUnauthorized)})
 	}
 
-	req := new(oapi.NumberReq)
-	if err := c.Bind(req); err != nil {
-		slog.ErrorContext(ctx, "Request Bind Error", "method", "PostNumber")
-		return c.JSON(http.StatusBadRequest, map[string]string{"message": http.StatusText(http.StatusBadRequest)})
-	}
-
-	if req.Num == nil {
-		slog.ErrorContext(ctx, "Wrong Request Body", "method", "PostNumber")
+	req, err := validateRequestBody(c)
+	if err != nil {
+		slog.ErrorContext(ctx, err.Error(), "method", "PostNumber")
 		return c.JSON(http.StatusBadRequest, map[string]string{"message": http.StatusText(http.StatusBadRequest)})
 	}
 
 	slog.InfoContext(ctx, "success", "method", "PostNumber")
 	return c.JSON(http.StatusOK, map[string]int{"num": (*req.Num) + 1})
+}
+
+func validateAPIKey(reqKey string) error {
+	var KeyError = errors.New("Wrong API Key")
+	cfg := env.LoadEnv()
+	if reqKey != cfg.APIKey {
+		return KeyError
+	}
+	return nil
+}
+
+func validateRequestBody(c echo.Context) (*oapi.NumberReq, error) {
+	req := new(oapi.NumberReq)
+	if err := c.Bind(req); err != nil {
+		return nil, &RequestBindError{Message: "Request Bind Error"}
+	}
+
+	if req.Num == nil {
+		return nil, &MissingRequestError{Message: "Missing Request Body"}
+	}
+	return req, nil
+}
+
+type RequestBindError struct {
+	Message string
+}
+
+func (e *RequestBindError) Error() string {
+	return e.Message
+}
+
+type MissingRequestError struct {
+	Message string
+}
+
+func (e *MissingRequestError) Error() string {
+	return e.Message
 }
